429b052921fb9f5948633495ff86b4c3a3ed3875
Anticoder03 <ap5381545@gmail.com>
2026-06-08 19:11:14 +0530

    Exception

---
+++ ExceptionDemo.java
@@ -0,0 +1,16 @@
+package day11;
+
+public class ExceptionDemo {
+	public static void main(String[] args) {
+		int a=10,b=0;
+//		int c = a/b;
+//		System.out.println(c);
+		
+		int arr[] = {1,2,3,4,5,6};
+//		System.out.println(arr[7]);
+		
+		String name = null;
+		System.out.println(name.length());
+		
+	}
+}
